aaae12d3e1c02744caa27ef0555d599627302cd3
Kwu564 <tnkboy4@gmail.com>
2018-02-02 22:02:52 -0800

    tweaking things

--- TestFishing/Assets/TileMove.cs
+++ TestFishing/Assets/TileMove.cs
@@ -6,37 +6,61 @@ public class TileMove : MonoBehaviour {
     public int current = 0;
     private int steps = 1;
     public float speed = 1;
+    public float smoothing;
     private Side map;
+    public GameObject cam;
     public GameObject wharf;
-	// Use this for initialization
-	void Start () {
+    public GameObject OtherPlayer;
+    public Vector3 End;
+    private bool moving;
+    // Use this for initialization
+    void Start () {
         map = wharf.GetComponent<Side>();
+        End = transform.position;
 	}
-	
+
     void Update()
     {
+        if (moving && Vector3.Distance(transform.position, End) > smoothing)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, End, speed * Time.deltaTime);
+        }else if (moving)
+        {
+            print("End of player turn");
+            print(OtherPlayer.GetComponent<TileMove>());
+            cam.GetComponent<CameraFollow>().target = OtherPlayer.transform;
+            OtherPlayer.GetComponent<TileMove>().enabled = true;
+            this.enabled = false;
+            moving = false;
+        }
         if (Input.GetMouseButtonDown(0))
         {
             steps = (int)Random.Range(1, 6);
             Debug.Log(steps);
+            //target = map.NextTile(current + steps);
+            //current = current + steps;
+            //print(target);
             move();
         }
-        
-        
-
     }
 	// Update is called once per frame
 	void move () {
-        Vector3 End = map.NextTile(current + 1);
-        while (Vector3.Distance(transform.position,End) > .0001)
+        if (moving)
+        {
+            return;
+        }
+        End = map.NextTile(current + steps);
+        moving = true;
+        current = current + steps;
+        /*while (Vector3.Distance(transform.position,End) > .0001)
         {
             transform.position = Vector3.MoveTowards(transform.position, End, speed * Time.deltaTime);
-        }
-        current = current + 1;
-        steps--;
-        if (steps > 0)
+        }*/
+       // current = current + 1;
+        //steps--;
+        /*if (steps > 0)
         {
             Invoke("move", 1f);
-        }
+        }*/
     }
 }
