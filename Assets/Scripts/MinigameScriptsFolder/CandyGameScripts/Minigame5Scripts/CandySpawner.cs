3ddef406f9165347dc52442cc290ebf633d77c3d
Janelynn Camingue <greentomboy@sbcglobal.net>
2018-02-23 20:27:08 -0800

    Fixed spawner bug

--- Assets/Scripts/MinigameScriptsFolder/CandyGameScripts/Minigame5Scripts/CandySpawner.cs
+++ Assets/Scripts/MinigameScriptsFolder/CandyGameScripts/Minigame5Scripts/CandySpawner.cs
@@ -6,7 +6,10 @@ public class CandySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject candy;
-	public float spawnTime = 4f;
+	public List<GameObject> candies = new List<GameObject> ();
+	public float spawnTime;
+	public float lowX;
+	public float highX;
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating("Spawn",0f, spawnTime);
@@ -14,8 +17,9 @@ public class CandySpawner : MonoBehaviour {
 
 	// Update is called once per frame
 	void Spawn () {
-		float x = Random.Range(-30.0f, 30f);
+		float x = Random.Range(lowX,highX);
+		int index = (int)Random.Range (0f, (float)candies.Count);
 		//float y = Random.Range(0f, 9.0f);
-		Instantiate(candy, new Vector3(x, 9.0f, 0),Quaternion.Euler(0f,0f,0f));
+		Instantiate(candies[index], new Vector3(x, 9.0f, 0),Quaternion.Euler(0f,0f,0f));
 	}
 }
---
+++ Assets/Scripts/MinigameScriptsFolder/CandyGameScripts/Minigame5Scripts/PlayerCandyBoxMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCandyBoxMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveBackAndForth ();
+		
+	}
+	void MoveBackAndForth(){
+		if (Input.GetKeyDown ("left")) {
+			Debug.Log ("Left");
+		}
+		if (Input.GetKeyDown ("right")) {
+			Debug.Log ("Right");
+		}
+	}
+}
