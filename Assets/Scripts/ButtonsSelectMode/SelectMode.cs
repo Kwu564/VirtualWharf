67f622a7bda98448ca8c32369e1dc165772a21d3
Janelynn Camingue <greentomboy@sbcglobal.net>
2018-03-03 20:44:31 -0800

    Added functionality for select mode

---
+++ Assets/Scripts/ButtonsSelectMode/SelectMode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SelectMode : MonoBehaviour {
+	public Button OnePlayer;
+	public Button TwoPlayer;
+
+	// Use this for initialization
+	void Start () {
+		OnePlayer.onClick.AddListener (ToOnePlayerMode);
+		TwoPlayer.onClick.AddListener (ToTwoPlayerMode);
+	}
+	void ToOnePlayerMode(){
+		SceneManager.LoadScene ("sceneOne");
+	}
+	void ToTwoPlayerMode(){
+		print ("to scene");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
